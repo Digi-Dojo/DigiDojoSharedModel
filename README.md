367c72a8cf3a63853ab364dd04917e4af2a916b7
Thiago Guerrero <tgbalera@amazon.com>
2023-10-06 16:10:22 -0300

    Init gradle package
    Signed-off-by: Thiago Guerrero <tgbalera@amazon.com>

---
+++ README.md
@@ -0,0 +1,39 @@
+# DigiDojoSharedModel
+
+This repository contains shared resources in order to be used by the microservices on internal communication in the Digi-Dojo project.
+
+## Requirements
+
+- Java 17 or higher
+- Gradle
+
+## Getting Started
+
+To build the project run:
+
+```
+gradle build
+```
+
+To run tests, execute the following command in the project root:
+```
+gradle test
+```
+
+This will run all test classes in the `src/test/java` directory.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    id("java")
+    id("jacoco")
+    id("checkstyle")
+    id("com.github.spotbugs") version "6.0.0-beta.3"
+}
+
+group = "it.unibz.digidojo"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("junit:junit:4.13.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+}
+
+spotbugs {
+    ignoreFailures.set(false)
+//    excludeFilter.set(project.file("${project.projectDir}/configuration/spotbugs/spotbugs-filters.xml"))
+}
+
+val excludeFromCoverage = listOf(
+        "it/unibz/digidojo/**/model/**",
+)
+
+val codeCoverageFiles: FileTree = sourceSets.main.get().output.asFileTree.matching {
+    exclude(excludeFromCoverage)
+}
+
+tasks {
+    withType<JacocoReport> {
+        classDirectories.setFrom(codeCoverageFiles)
+    }
+
+    jacocoTestCoverageVerification {
+        /*
+         Files to include in code coverage evaluations and reports
+         */
+        classDirectories.setFrom(codeCoverageFiles)
+
+        /*
+         Set minimum code coverage to fail build
+        */
+        violationRules {
+            rule { limit { minimum = BigDecimal.valueOf(0.9) } }
+        }
+    }
+
+    check {
+        dependsOn(jacocoTestCoverageVerification)
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+
+        testLogging {
+            events = setOf(TestLogEvent.FAILED,
+                    TestLogEvent.PASSED,
+                    TestLogEvent.SKIPPED)
+            exceptionFormat = TestExceptionFormat.FULL
+            showCauses = true
+            showExceptions = true
+            showStackTraces = true
+            showStandardStreams = true
+        }
+    }
+}
